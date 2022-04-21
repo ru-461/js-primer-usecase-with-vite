c42a189f06c6f557b64620b3facbc79c341c06d5
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-04-21 20:41:52 +0900

    update: Add DOM Sample

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,1 +1,9 @@
 console.log("index.js: loaded");
+
+// DOM Sample
+const heading = document.querySelector('h2');
+const headngText = heading.textContent;
+const button = document.createElement('button');
+button.textContent = 'Push Me';
+
+document.body.appendChild(button);
