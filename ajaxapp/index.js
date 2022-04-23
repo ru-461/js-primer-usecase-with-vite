e0565504105d7cb6aafb1348ed1acdab310287f7
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-04-23 16:43:41 +0900

    update:Section 3

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -5,6 +5,26 @@ const heading = document.querySelector('h2');
 const headngText = heading.textContent;
 const button = document.createElement('button');
 
+function escapeSpecialChars(str) {
+  return str
+  .replace(/&/g, "&amp;")
+  .replace(/</g, "&lt;")
+  .replace(/>/g, "&gt;")
+  .replace(/"/g, "&quot;")
+  .replace(/'/g, "&#039;");
+}
+
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+}
+
 // Fetch API
 function fetchUserInfo(userId) {
   fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
@@ -14,7 +34,19 @@ function fetchUserInfo(userId) {
       console.error("エラーレスポンス", response);
     } else {
       return response.json().then(userInfo => {
-        console.log(userInfo);
+        // template literal
+        const view = escapeHTML`
+        <h4>${userInfo.name} (@${userInfo.login})</h4>
+        <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+        <dl>
+            <dt>Location</dt>
+            <dd>${userInfo.location}</dd>
+            <dt>Repositories</dt>
+            <dd>${userInfo.public_repos}</dd>
+        </dl>
+        `;
+        const result = document.getElementById('result');
+        result.innerHTML = view;
       });
     }
   }).catch(error => {
