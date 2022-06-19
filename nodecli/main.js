6ea17b9b17ec1693b2d8d9793634e3491282fd60
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-06-19 22:04:23 +0900

    update: Complete argument-parse

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+// console.log('Hello World');
+// console.log(process.argv);
+
+// Import commander
+const program = require("commander");
+
+// Parse
+program.parse(process.argv);
+
+// Filepath
+const filePath = program.args[0];
+
+// Output
+console.log(filePath);
