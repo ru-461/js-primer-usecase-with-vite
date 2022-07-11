ffbe6517400fc1c35426cde1d4eb6da07faaf753
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-07-11 20:29:03 +0900

    update: Section 3 read file

--- nodecli/main.js
+++ nodecli/main.js
@@ -4,6 +4,10 @@
 // Import commander
 const program = require("commander");
 
+// Import fs
+const fs = require("fs");
+const { exit } = require("process");
+
 // Parse
 program.parse(process.argv);
 
@@ -11,4 +15,14 @@ program.parse(process.argv);
 const filePath = program.args[0];
 
 // Output
-console.log(filePath);
+// console.log(filePath);
+
+// Read Markdown file
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+  if (err) {
+    console.log(err.message);
+    process.exit(1);
+    return;
+  }
+  console.log(file);
+});
